307fcd38add17e6aeaa99cb6963ba3b45f8f37a9
0shreyajha0 <0shreyajha0@gmail.com>
2024-01-27 18:12:47 +0530

    changes revert

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,11 +10,6 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const socialMediaLinks = [
   { href: "https://twitter.com/_Shreyajha", icon: faTwitter },
-  { href: "https://medium.com/@0shreyajha0", icon: faMedium },
-  {
-    href: "https://www.linkedin.com/in/shreya-jha-b166a1212/",
-    icon: faLinkedin,
-  },
   {
     href: "https://www.linkedin.com/in/shreya-jha-b166a1212/",
     icon: faLinkedin,
